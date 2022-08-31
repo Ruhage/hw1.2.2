60389cfff5a9df1a3edcc65102203bc04f2fe788
Ruhge <ruhage26@gmail.com>
2022-08-31 15:23:03 +1000

    hw1.2.2

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
+
+        // Задача 2
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+
+
+    }
+}
